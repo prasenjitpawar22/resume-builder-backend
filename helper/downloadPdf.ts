7638f2ccbe05480e78282e345a2fdc420aad11aa
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-04-14 12:04:54 +0530

    still trying

--- helper/downloadPdf.ts
+++ helper/downloadPdf.ts
@@ -2,6 +2,9 @@
 let chrome: any = {};
 let puppeteer: any;
 
+console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
+
+
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     // running on the Vercel platform.
     chrome = require('chrome-aws-lambda');
@@ -16,54 +19,48 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 export const downloadPdf = async (userId: string) => {
     // Create a browser instance
     // const browser = await puppeteer.launch();
-    let browser = await puppeteer.launch({
-        args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-    });
-    // Create a new page
-    const page = await browser.newPage();
+    try {
+        let browser = await puppeteer.launch({
+            args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        });
+        // Create a new page
+        const page = await browser.newPage();
 
-    // Website URL to export as pdf
-    let website_url = ''
-    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-        website_url = `https://backend-be.vercel.app/build/download/${userId}`;
+        // Website URL to export as pdf
+        let website_url = ''
+        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+            website_url = `https://backend-be.vercel.app/build/download/${userId}`;
+        }
+        else {
+            website_url = `http://localhost:8000/build/download/${userId}`
+        }
+
+        // Open URL in current page
+        await page.goto(website_url, { waitUntil: 'networkidle0' });
+        console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
+
+        await page.emulateMediaType('screen');
+
+
+        const pdf = await page.pdf({
+            path: 'result.pdf',
+            format: 'A4',
+        });
+
+
+        await browser.close();
+
+        return pdf
+    } catch (err) {
+        console.error(err);
+        return null;
     }
-    else {
-        website_url = `http://localhost:8000/build/download/${userId}`
-    }
 
-    // Open URL in current page
-    await page.goto(website_url, { waitUntil: 'networkidle0' });
-    console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
-
-    // //Get HTML content from HTML file
-    // const html = fs.readFileSync('sample.html', 'utf-8');
-    // await page.setContent(html, { waitUntil: 'networkidle2' });
-
-    //To reflect CSS used for screens instead of print
-    await page.emulateMediaType('screen');
-
-    // Downlaod the PDF
-    const pdf = await page.pdf({
-        path: 'result.pdf',
-        // margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-        // printBackground: true,
-        format: 'A4',
-    });
-
-    // Capture screenshot
-    await page.screenshot({
-        path: 'screenshot.jpg'
-    });
-    // Close the browser instance
-    await browser.close();
-
-    return pdf
 }
-
 
 // const getData = async (url) => {
 //   try {
