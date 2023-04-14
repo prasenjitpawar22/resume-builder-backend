fd5a93f43a2d2a8cc74e173eb10be89e7541c1a1
Prasenjit Pawar <prasenjit.pawar@capgemini.com>
2023-04-14 12:52:08 +0530

    still try mf

--- helper/downloadPdf.ts
+++ helper/downloadPdf.ts
@@ -1,18 +1,20 @@
 // // import fs from 'fs'
-let chrome: any = {};
-let puppeteer: any;
+// let chrome: any = {};
+// let puppeteer: any;
 
-console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
+// console.log(process.env.AWS_LAMBDA_FUNCTION_VERSION);
 
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    // running on the Vercel platform.
-    chrome = require('chrome-aws-lambda');
-    puppeteer = require('puppeteer-core');
-} else {
-    // running locally.
-    puppeteer = require('puppeteer');
-}
+// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+//     // running on the Vercel platform.
+//     chrome = require('chrome-aws-lambda');
+//     puppeteer = require('puppeteer-core');
+// } else {
+//     // running locally.
+//     puppeteer = require('puppeteer');
+// }
+import chromium from 'chrome-aws-lambda';
+
 
 // import puppeteer from 'puppeteer'
 
@@ -20,24 +22,24 @@ export const downloadPdf = async (userId: string) => {
     // Create a browser instance
     // const browser = await puppeteer.launch();
     try {
-        let browser = await puppeteer.launch({
-            args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
+        let browser = await chromium.puppeteer.launch({
+            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
             headless: true,
             ignoreHTTPSErrors: true,
-        });
+        })
         // Create a new page
         const page = await browser.newPage();
 
         // Website URL to export as pdf
-        let website_url = ''
-        if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-            website_url = `https://backend-be.vercel.app/build/download/${userId}`;
-        }
-        else {
-            website_url = `http://localhost:8000/build/download/${userId}`
-        }
+        // let website_url = ''
+        // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        let website_url = `https://backend-be.vercel.app/build/download/${userId}`;
+        // }
+        // else {
+        // website_url = `http://localhost:8000/build/download/${userId}`
+        // }
 
         // Open URL in current page
         await page.goto(website_url, { waitUntil: 'networkidle0' });
@@ -48,7 +50,7 @@ export const downloadPdf = async (userId: string) => {
 
         const pdf = await page.pdf({
             path: 'result.pdf',
-            format: 'A4',
+            format: 'a4',
         });
 
 
